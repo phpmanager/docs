1176adf087b43beede1352bfeaf959e82574d1ab
Lex Li <support@lextm.com>
2024-03-19 00:30:38 -0400

    Updated analytics ID.

--- conf.py
+++ conf.py
@@ -400,5 +400,5 @@ def setup(app):
         """Insert Google Analytics tracker
         Based on this Stackoverflow suggestion: https://stackoverflow.com/a/41885884
         """
-        app.add_js_file("https://www.googletagmanager.com/gtag/js?id=G-H1944HS5X8")
+        app.add_js_file("https://www.googletagmanager.com/gtag/js?id=G-DFLYZZK12P")
         app.add_js_file("google_analytics_tracker.js")
